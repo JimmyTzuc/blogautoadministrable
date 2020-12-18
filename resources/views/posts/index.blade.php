a1d41aa057702d4a087d89b5f98b03117b827c16
jimmy tzuc <stjimmytzuc@gmail.com>
2020-12-18 14:01:09 -0600

    tags ajustados por secciones y colores en la vista

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
 
     <div class="container py-8 border">
-        <div class="grid grid-cols-3 gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
-                <article class="@if($loop->first) col-span-2 @endif w-full h-80 bg-cover bg-center" style="background-image: url({{Storage::url($post->image->url)}})">
+                <article class="@if($loop->first) md:col-span-2 @endif w-full h-80 bg-cover bg-center" style="background-image: url({{Storage::url($post->image->url)}})">
                     <div class="w-full h-full px-8 flex flex-col justify-center">
 
                         <div>
                         @foreach($post->tags as $tag)
-                            <a class="inline-block px-3 h-6 bg-gray-600 text-white rounded-full" href="">{{$tag->name}}</a>
+                            <a class="inline-block px-3 h-6 bg-{{$tag->color}}-600 text-white rounded-full" href="">{{$tag->name}}</a>
                         @endforeach
                         </div>
 
@@ -18,6 +18,10 @@
                     </div>
                 </article>
             @endforeach
+        </div>
+
+        <div class="mt-4">
+            {{$posts->links()}}
         </div>
     </div>
 

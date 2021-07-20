24558903c9eac5b56db848f7c9eb82392542c9ca
Jimmy Tzuc <stjimmytzuc@gmail.com>
2021-07-20 09:18:14 -0500

    modified home on navigation

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -69,6 +69,7 @@
         <div class="px-2 pt-2 pb-3 space-y-1">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <!-- <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a> -->
+            <a href="/" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Inicio</a>
             @foreach ($categories as $category)
             <a href="{{route('posts.category',$category)}}" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">{{$category->name}}</a>
             @endforeach
